3f0e2019c4b6a9c2a848766b7224aaa01b222b9e
Rich <r.pangborn@methodintegration.com>
2014-07-24 16:45:44 -0400

    ad

---
+++ SharedRepo/RepoShare.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedRepo
+{
+    public class RepoShare
+    {
+        public string value;
+        public int number;
+
+        public void ReposShare()
+        {
+            value = "Initial value";
+            number = 0;
+        }
+
+        public void setValue(string val)
+        {
+            value = val;
+        }
+        public void setNumber(int num)
+        {
+            number = num;
+        }
+        public string getValue() { return value; }
+        public int getNumber() { return number; }
+    }
+}
